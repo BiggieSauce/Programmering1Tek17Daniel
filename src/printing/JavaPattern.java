558408f45c11700f0b1d3f600274e5b4d3800c21
BiggieSauce <42860873+BiggieSauce@users.noreply.github.com>
2018-09-05 14:45:15 +0200

    JavaPattern

---
+++ src/printing/JavaPattern.java
@@ -0,0 +1,13 @@
+package printing;
+
+public class JavaPattern {
+
+	public static void main(String[] args) {
+		System.out.print("     J    a   v     v  a                                                  \r\n" + 
+				"     J   a a   v   v  a a                                                 \r\n" + 
+				"  J  J  aaaaa   V V  aaaaa                                                \r\n" + 
+				"   JJ  a     a   V  a     a");
+			 
+	}
+
+}
