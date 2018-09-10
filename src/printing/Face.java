4730a4e0851db07d08bf61edd69105d25d1ec21c
BiggieSauce <42860873+BiggieSauce@users.noreply.github.com>
2018-09-10 11:24:46 +0200

    Face

---
+++ src/printing/Face.java
@@ -0,0 +1,15 @@
+package printing;
+
+public class Face {
+
+	public static void main(String[] args) {
+			System.out.print("+\"\"\"\"\"+                                                 \r\n" + 
+					       "  [| o o |]                                                \r\n" + 
+					       "   |  ^  |                                                 \r\n" + 
+					       "   | '-' |                                                 \r\n" + 
+					       "   +-----+");                                                
+  
+
+	}
+
+}
