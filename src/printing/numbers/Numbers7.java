183862610307c7043f48b2a072abd6a89e370171
BiggieSauce <42860873+BiggieSauce@users.noreply.github.com>
2018-09-14 09:37:25 +0200

    Numbers 7

---
+++ src/printing/numbers/Numbers7.java
@@ -0,0 +1,11 @@
+package printing.numbers;
+
+public class Numbers7 {
+
+	public static void main(String[] args) {
+		System.out.println("Area is 5.5 * 8.5 = " + 5.5 * 8.5);
+		System.out.println("Perimeter is 2 * (5.6 + 8.5) = " + 2 * (5.6 + 8.5));
+
+	}
+
+}
