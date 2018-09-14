be35550c63d3e8726593548df8ef6d9430accca7
BiggieSauce <42860873+BiggieSauce@users.noreply.github.com>
2018-09-14 09:46:04 +0200

    Numbers 6

---
+++ src/printing/numbers/Numbers6.java
@@ -0,0 +1,11 @@
+package printing.numbers;
+
+public class Numbers6 {
+
+	public static void main(String[] args) {
+		System.out.println("Perimeter is = " + Math.PI * 7.5 * 2);
+		System.out.println("Area is = " + Math.PI * 7.5 * 7.5);
+
+	}
+
+}
